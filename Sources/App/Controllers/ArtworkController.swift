0d6a0fdf41790e21273cb4fa134a6f0e67b0c55e
Balystick <balystick74@icloud.com>
2024-10-21 16:24:23 +0200

    running

---
+++ Sources/App/Controllers/ArtworkController.swift
@@ -0,0 +1,29 @@
+//
+//  ArtworkController.swift
+//  Tag-It-Server
+//
+//  Created by Aurélien on 17/10/2024.
+//
+
+import Vapor
+import Fluent
+
+struct ArtworkController: RouteCollection {
+        func boot(routes: RoutesBuilder) throws {
+            let artworks = routes.grouped("artworks")
+            artworks.get(use: index)
+            artworks.post(use: create)
+            
+            @Sendable
+            func index(req: Request) async throws -> [Artwork] {
+                return try await Artwork.query(on: req.db).all()
+            }
+            
+            @Sendable
+            func create(req: Request) async throws -> Artwork {
+                let artworks = try req.content.decode(Artwork.self)
+                try await artworks.save(on: req.db)
+                return artworks
+        }
+    }
+}
---
+++ Sources/App/Models/Artwork.swift
@@ -0,0 +1,48 @@
+//
+//  Artwork.swift
+//  Tag-It-Server
+//
+//  Created by Aurélien on 17/10/2024.
+//
+import Vapor
+import Fluent
+import struct Foundation.UUID
+
+final class Artwork: Model, Content, @unchecked Sendable {
+    static let schema = "artworks"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "title")
+    var title: String?
+    
+    @Field(key: "image" )
+    var image: String?
+    
+    @Field(key: "type" )
+    var type: String?
+    
+    @Field(key: "address" )
+    var address: String?
+    
+    @Field(key: "city" )
+    var city: String?
+    
+    @Field(key: "country" )
+    var country: String?
+    
+    @Field(key: "latitude" )
+    var latitude: Double?
+    
+    @Field(key: "longitude" )
+    var longitude: Double?
+    
+    @Field(key: "points" )
+    var points: String?
+    
+    @Field(key: "id_artist" )
+    var id_artist: UUID?
+
+    init() { }
+}
