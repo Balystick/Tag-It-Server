c1cf7ef4a65ee1a44d17abc7d39da3b16ef781ca
Marc <marcdvlpr@gmail.com>
2024-10-22 12:28:31 +0200

    Add getArtworkByID method to ArtworkController

--- Sources/App/Controllers/ArtworkController.swift
+++ Sources/App/Controllers/ArtworkController.swift
@@ -14,6 +14,10 @@ struct ArtworkController: RouteCollection {
             artworks.get(use: index)
             artworks.post(use: create)
             
+            artworks.group(":artworkID") { artwork in
+                artwork.get(use: self.getArtworkByID)
+            }
+            
             @Sendable
             func index(req: Request) async throws -> [Artwork] {
                 return try await Artwork.query(on: req.db).all()
@@ -25,5 +29,15 @@ struct ArtworkController: RouteCollection {
                 try await artworks.save(on: req.db)
                 return artworks
         }
+    }
+}
+
+extension ArtworkController {
+    @Sendable
+    func getArtworkByID(req: Request) async throws -> Artwork {
+        guard let artwork = try await Artwork.find(req.parameters.get("artworkID"), on: req.db) else {
+            throw Abort(.notFound, reason: "Artwork doesn't exist.")
+        }
+        return artwork
     }
 }
