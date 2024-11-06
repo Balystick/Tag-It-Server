33430804daca3106aade2bd40719af013a30df22
Marc <marcdvlpr@gmail.com>
2024-11-06 14:26:22 +0100

    Reset branch dev

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -7,23 +7,3 @@
 import Vapor
 import Fluent
 
-final class User: Model, Content {
-    static let schema = "users"
-    
-    @ID(key: .id)
-    var id: UUID?
-    
-    @Field(key: "name")
-    var name: String
-    
-    @Field(key: "profile_image")
-    var profileImage: String?
-
-    init() {}
-    
-    init(id: UUID? = nil, name: String, profileImage: String? = nil) {
-        self.id = id
-        self.name = name
-        self.profileImage = profileImage
-    }
-}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,21 +2,3 @@ import NIOSSL
 import FluentMySQLDriver
 import Vapor
 
-// configures your application
-public func configure(_ app: Application) async throws {
-
-    app.http.server.configuration.port = 8080
-
-    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(DatabaseConfigurationFactory.mysql(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "root",
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? "tag_it"
-    ), as: .mysql)
-
-    // register routes
-    try routes(app)
-}
