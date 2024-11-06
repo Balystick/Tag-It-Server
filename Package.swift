90d773b2ee1dfd6269fd4fd0239007e3f215aea0
Marc <marcdvlpr@gmail.com>
2024-11-06 15:15:12 +0100

    Add CORS and a rate limiter

--- Package.swift
+++ Package.swift
@@ -16,7 +16,9 @@ let package = Package(
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
         // JWT
-        .package(url: "https://github.com/vapor/jwt.git", from: "5.0.0-rc")
+        .package(url: "https://github.com/vapor/jwt.git", from: "5.0.0-rc"),
+        // Gatekeeper
+        .package(url: "https://github.com/nodes-vapor/gatekeeper.git", from: "4.0.0")
     ],
     targets: [
         .executableTarget(
@@ -27,7 +29,8 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
-                .product(name: "JWT", package: "jwt")
+                .product(name: "JWT", package: "jwt"),
+                .product(name: "Gatekeeper", package: "gatekeeper")
             ],
             swiftSettings: swiftSettings
         ),
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,6 +3,7 @@ import Fluent
 import FluentMySQLDriver
 import Vapor
 import JWT
+import Gatekeeper
 
 public func configure(_ app: Application) async throws {
     app.http.server.configuration.port = 8080
@@ -27,6 +28,23 @@ public func configure(_ app: Application) async throws {
     }
     let hmacKey = HMACKey(from: Data(secret.utf8))
     await app.jwt.keys.add(hmac: hmacKey, digestAlgorithm: .sha256)
+    
+    // CORS
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin : .all,
+        allowedMethods: [.GET, .POST, .PUT, .DELETE, .OPTIONS],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin],
+        cacheExpiration: 8
+    )
+    let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
+    
+    // Rate Limiter
+    app.caches.use(.memory)
+    app.gatekeeper.config = .init(maxRequests: 100, per: .minute)
+    
+    // Middleware
+    app.middleware.use(corsMiddleware)
+    app.middleware.use(GatekeeperMiddleware())
 
     // Register Routes
     try routes(app)
